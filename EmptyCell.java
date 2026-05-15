f9bb29351dc0ed58b6119d1bd7cf0a3c0d8077b2
Alp Aksel <alpaksel28@gmail.com>
2026-05-16 00:18:10 +0300

    Restructure project with src and package organization

--- EmptyCell.java
+++
@@ -1,6 +0,0 @@
-public class EmptyCell extends Cell {
-    public EmptyCell(int x, int y) {
-        super(x,y,'E');
-
-    }
-}
--- SE116ConfigurationException.java
+++
@@ -1,5 +0,0 @@
-public class SE116ConfigurationException extends RuntimeException {
-    public SE116ConfigurationException(String message) {
-        super(message);
-    }
-}
