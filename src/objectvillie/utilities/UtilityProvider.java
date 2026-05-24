0adb4c311ed3db6f198ef638c30ccf3de655d7b1
alginmustafakemal <alginmustafakemal@gmail.com>
2026-05-24 18:09:56 +0300

    Update UtilityProviders.java
    Add PowerPlant WaterPumpingStation InternetHub and create constructer overrided getSymbol ,gets  and consumption method

---
+++ src/objectvillie/utilities/UtilityProvider.java
@@ -0,0 +1,81 @@
+package src.objectvillie.utilities;
+
+public abstract class UtilityProvider {
+    protected int maxCapacity; // 100
+    protected int currentCapacity;
+    protected int x;
+    protected int y;
+
+    public UtilityProvider(  int x, int y) {
+        this.maxCapacity = 100;
+        this.currentCapacity = 100;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public int getCurrentCapacity() {
+        return currentCapacity;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void resetCapacity() {//For generating 100 utilitiy every tick
+        currentCapacity =maxCapacity;
+    }
+
+    public void consumeUtility(int amount){ // Conusmes utility
+        if (currentCapacity >= amount){
+            currentCapacity-=amount;
+        }else {
+            currentCapacity=0;
+        }
+    }
+
+    public abstract char getSymbol();
+}
+
+class PowerPlant extends UtilityProvider{
+    public PowerPlant( int x, int y) {
+        super( x, y);
+    }
+
+    @Override
+    public char getSymbol(){
+        return 'P';
+    }
+}
+class WaterPumpingStation extends UtilityProvider{
+    public WaterPumpingStation( int x, int y) {
+        super( x, y);
+    }
+
+    @Override
+    public char getSymbol(){
+        return 'W';
+    }
+
+}
+class InternetHub extends UtilityProvider{
+    public InternetHub( int x, int y) {
+        super( x, y);
+    }
+
+    @Override
+    public char getSymbol() {
+        return 'T';
+    }
+}
+
+
+
+
