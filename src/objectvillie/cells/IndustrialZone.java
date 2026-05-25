0d03426a9985e6c557ea100dff083dd6cd0a4c35
Alp Aksel <alpaksel28@gmail.com>
2026-05-25 18:05:15 +0300

    Reapply Zone class changes after merge and update other zone classes accordingly

--- src/objectvillie/cells/IndustrialZone.java
+++ src/objectvillie/cells/IndustrialZone.java
@@ -1,7 +1,9 @@
 package src.objectvillie.cells;
 
-public class IndustrialZone extends Cell {
-    public IndustrialZone(int x, int y, char symbol) {
-        super(x, y, symbol);
+public class IndustrialZone extends Zone {
+
+    public IndustrialZone(int x, int y, char symbol, int level, int electricity, int water, int internet, int population, int goods, int lifestyle, boolean hasSecurity, boolean hasHealth, boolean hasEducation, int output) {
+
+        super(x, y, symbol, level, electricity, water, internet, population, goods, lifestyle, hasSecurity, hasHealth, hasEducation, output);
     }
 }
