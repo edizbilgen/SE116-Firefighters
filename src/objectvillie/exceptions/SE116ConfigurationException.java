7c501726fbd03dec9da39d4a59a992f26a0e7cb8
RAGIP EDİZ BİLGEN <edizbilgen1@gmail.com>
2026-05-27 18:37:05 +0300

    Delete src/objectvillie/exceptions/SE116ConfigurationException.java

--- src/objectvillie/exceptions/SE116ConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package src.objectvillie.exceptions;
-
-public class SE116ConfigurationException extends RuntimeException {
-    public SE116ConfigurationException(String message) {
-        super(message);
-    }
-}
