21f28427d03e37f8943c90ca242ad78c5a05c439
RAGIP EDİZ BİLGEN <edizbilgen1@gmail.com>
2026-05-31 22:18:17 +0300

    Deleted

--- src/objectvillie/utilities/BFSengine.java
+++ src/objectvillie/utilities/BFSengine.java
@@ -24,42 +24,7 @@ public class BFSengine {
 
      while (utilityProvider.getCurrentCapacity()>0 && !cells.isEmpty()){
          Cell currentCell=cells.poll();
-
-         if(currentCell instanceof Zone){
-             Zone currentzone = (Zone) currentCell;
-             int demand = currentzone.calculateUtilityDemand();
-             int availableCapacity = utilityProvider.getCurrentCapacity();
-
-             int allocatedAmount = Math.min(demand,availableCapacity);
-
-             char type = utilityProvider.getSymbol();
-             if (type == 'P'){
-                 currentzone.addElectricity(allocatedAmount);
-             }
-             else if (type == 'W'){
-                 currentzone.addWater(allocatedAmount);
-             }
-             else if (type == 'T') {
-                 currentzone.addInternet(allocatedAmount);
-             }
-             utilityProvider.consumeUtility(allocatedAmount);
-         }
-         for (int i = 0; i < 4; i++) {
-             int nextRow = currentCell.getX() + dRow[i];
-             int nextCol = currentCell.getY() + dCol[i];
-
-             Cell nextCell = cityMap.getCell(nextRow, nextCol);
-             if(nextCell != null && !visited[nextRow][nextCol] && !(nextCell instanceof EmptyCell)){
-                 cells.add(nextCell);
-                 visit(nextCell,visited);
-             }
-         }
          
-         if(currentCell instanceof Zone){
-             
-         }
-             
-
      }
  }
 }
